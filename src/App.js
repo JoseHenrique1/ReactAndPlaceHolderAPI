8f17024c6df28d16422d1aa7af3867256aa052e3
Jose Henrique <jh4832184@gmail.com>
2023-11-05 20:52:50 -0300

    feat: add fetch data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [allPosts, setAllPosts] = useState([]);
+
+  const loadPosts = async ()=>{
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photosResponse]);
+
+    const postjson = await posts.json();
+    const photosjson = await photos.json();
+
+    const postandphotos = postjson.map((post, index)=>{
+      return {...post, cover: photosjson[index].url};
+    });
+
+    setAllPosts(postandphotos);
+  }
+
+  useEffect(loadPosts, []);
+
+
+
   return (
-    <div>
-      <h1>JsonPlaceHolder</h1>
-    </div>
+    <section className='container'>
+      <div className='posts'>
+          {allPosts.map( (item)=>{
+            return (
+              <div className='post'>
+                <img src={item.cover} alt={item.title}/>
+                <div className='post-content'>
+                  <h1>{item.title}</h1>
+                  <p>{item.body}</p>
+                </div>
+              </div>
+            )
+          })}
+      </div>
+    </section>
   );
 }
 
